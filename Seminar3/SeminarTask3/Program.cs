4b136f7bce59f4be9be1ffb4724c1627b377b344
Pahomob Artem <art.pa99@mail.ru>
2023-07-05 14:54:55 +0300

    Task3

---
+++ Seminar3/SeminarTask3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 18
+//Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+void PrintIntervals(int n)
+{
+        switch (n)
+        {
+        case 1:
+            Console.WriteLine("X > 0, y > 0");
+            break;
+        case 2:
+            Console.WriteLine("X < 0, y > 0");
+            break;
+        case 3:
+            Console.WriteLine("X < 0, y < 0");
+            break;
+        case 4:
+            Console.WriteLine("X > 0, y < 0");
+            break;
+        default:
+            Console.WriteLine("Error");
+            break;
+        }
+}
+//---------------------------------
+
+Console.Write("Please, integer number of quates: ");
+int Num = int.Parse(Console.ReadLine());
+
+PrintIntervals(Num);
