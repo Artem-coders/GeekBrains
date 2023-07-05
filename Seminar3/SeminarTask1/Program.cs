2699024dcfbbf9459c93d6263c72dcb29f934206
Pahomob Artem <art.pa99@mail.ru>
2023-07-05 14:28:03 +0300

    Task1 and Task2

---
+++ Seminar3/SeminarTask1/Program.cs
@@ -0,0 +1,21 @@
+﻿// [Возвразаемый тип] [Имя метод - глагол]([Параметры метода])
+//{
+    // код метод
+//}
+
+int GetSum(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
+
+
+void PrintMessageConsole(string message)
+{
+    Console.WriteLine(message);
+}
+
+// --------------------------------------------------
+
+int mySum = GetSum(10, 15);
+PrintMessageConsole(mySum.ToString());
