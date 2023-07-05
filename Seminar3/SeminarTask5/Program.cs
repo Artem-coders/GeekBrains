6c29c4b13b56fe32d9f6c82161c0af18ea03a436
Pahomob Artem <art.pa99@mail.ru>
2023-07-05 16:02:59 +0300

    Task5

---
+++ Seminar3/SeminarTask5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 22
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+//от 1 до N.
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+void GetSqrt(int N)
+{
+    for (int i = 1; i <= N; i++)
+    Console.Write($"{i * i}");
+}
+int N = Promt("Введите число: ");
+GetSqrt(N);
