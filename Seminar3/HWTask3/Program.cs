f08db7b41380ae4b1b25d9ff78c99f9b95766bed
Pahomob Artem <art.pa99@mail.ru>
2023-07-05 16:45:30 +0300

    HW3

---
+++ Seminar3/HWTask3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+void GetSqrt(int N)
+{
+    for (int i = 1; i <= N; i++)
+    Console.WriteLine($"{i * i * i}");
+}
+int N = Promt("Введите число: ");
+GetSqrt(N);
