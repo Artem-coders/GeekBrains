05f8231328066fff2cf13f4295765a58cadb579f
Pahomob Artem <art.pa99@mail.ru>
2023-07-10 14:18:51 +0300

    25

---
+++ Seminar4/HW25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Double x, y, result;
+        Console.Write("Введите число A: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Введите число B: ");
+        int b = int.Parse(Console.ReadLine());
+
+        result = Math.Pow(a, b);
+        Console.WriteLine($"Pow({a},{b}) = {result}");
+    }
+}
---
+++ Seminar4/SeminarTask5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выполнить бинарный поиск в массиве
+
+int [] Array = new int [8];
+
+int [] Random(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+    array[i] = new Random().Next(0,2);
+    Console.Write(Array[i]);
+    }
+    return array;
+    
+}
+
+int[] Array2 = Random(Array);
