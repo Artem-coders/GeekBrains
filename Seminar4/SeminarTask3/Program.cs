bf03074d7ee6c397b6f43a644b6e67052af68e5b
Pahomob Artem <art.pa99@mail.ru>
2023-07-10 13:32:14 +0300

    Task2

---
+++ Seminar4/SeminarTask3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+
+
+int multiply(int numUs)
+{
+    int temp = 1;
+    for (int i = 1; i <= numUs; i++)
+    {
+        temp = temp * i;
+    }
+    return temp;
+}
+
+int result = (multiply(5));
+Console.Write(result);
