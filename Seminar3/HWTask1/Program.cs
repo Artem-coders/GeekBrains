1be6e880b2413a1e310b677adf66cee96989c124
Pahomob Artem <art.pa99@mail.ru>
2023-07-10 13:19:42 +0300

    Task1

--- Seminar3/HWTask1/Program.cs
+++ Seminar3/HWTask1/Program.cs
@@ -7,22 +7,28 @@
 
 //-----------------------
 
-int num, r, sum=0, t;
-Console.Write("Введите пятизначное число: ");
-num = int.Parse(Console.ReadLine());
-if(num > 9999 && num < 100000)
+internal class Program
 {
-for(t=num; num!=0; num = num/10)
-{
-r = num % 10;
-sum = sum * 10 + r;
-}
-if(t==sum)
-{
-Console.Write("Палиндромом");
-}
-else
-{
-Console.Write("Не палиндромом");
-}
+    private static void Main(string[] args)
+    {
+        int num, r, sum = 0, t;
+        Console.Write("Введите пятизначное число: ");
+        num = int.Parse(Console.ReadLine());
+        if (num > 9999 && num < 100000)
+        {
+            for (t = num; num != 0; num = num / 10)
+            {
+                r = num % 10;
+                sum = sum * 10 + r;
+            }
+            if (t == sum)
+            {
+                Console.Write("Палиндромом");
+            }
+            else
+            {
+                Console.Write("Не палиндромом");
+            }
+        }
+    }
 }
