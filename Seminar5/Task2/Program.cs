f8e4ffc9328d94cbedb4c527f478f763c08539d8
Pahomob Artem <art.pa99@mail.ru>
2023-07-11 16:29:50 +0300

    Seminar5.2

---
+++ Seminar5/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] Printarray (int[] mas)
+{
+    Console.Write ("Исходный массив: ");
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = new Random().Next(-9, 10);
+        Console.Write(" " + mas[i]);
+    }
+    return mas;
+}
+
+void ChangeNum (int[] mas)
+{
+    Console.Write(" Новый массив: ");
+    for(int i = 0; i < mas.Length; i++)
+    {
+        mas[i] *= -1;
+        Console.Write(" " + mas[i]);
+    }   
+}
+
+int[] mas = new int[9];
+Printarray (mas);
+ChangeNum (mas);
---
+++ Seminar5/Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+//4; массив [6, 7, 19, 345, 3] -> нет
+//-3; массив [6, 7, 19, 345, 3] -> да
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] Filltarray (int[] mas)
+{
+    Console.Write ("Исходный массив: ");
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = new Random().Next(-9, 10);
+        Console.Write(" " + mas[i]);
+    }
+    return mas;
+}
+
+bool IsNumber (int[] mas, int num)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (num == mas[i])
+            return true;
+    }
+    return false;
+}
+int[] mas = new int[10];
+Filltarray(mas);
+int num = Prompt(" Введите число: ");
+if (IsNumber(mas, num) == true)
+    Console.Write ("Такое число есть в массиве: ");
+else 
+    Console.Write ("Такого числа нет массиве: ");
