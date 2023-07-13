2044af23f81abcda148edc926631bfd06d9f123d
Pahomob Artem <art.pa99@mail.ru>
2023-07-13 14:35:35 +0300

    34

---
+++ EXM1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вид 4
+string Method4(int count, string text)
+{
+    string result = "";
+    for(int i = 0; i < count; i++ )
+    {
+        result += text;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.Write(res);
+
+//цикл в цикле
---
+++ Seminar5/HW34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+void Filltarray (int[] mas)
+{
+    Console.Write ("Исходный массив: ");
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = new Random().Next(100, 1000);
+        Console.Write($"{mas[i]} ");
+    }
+}
+
+int kol (int[] mas)
+{
+int kol = 0;
+for (int i = 0; i < mas.Length; i++)
+{
+    if(mas[i] % 2 == 0)
+    kol++;
+    
+}
+    return kol;
+}
+
+int[] mas = new int[4];
+Filltarray (mas);
+Console.Write ($"Колличество чётных чисел в массиве: {kol(mas)}");
