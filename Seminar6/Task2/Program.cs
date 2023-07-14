89d047aacfda81a0195a0fab607f1572116971a6
Pahomob Artem <art.pa99@mail.ru>
2023-07-14 23:21:43 +0300

    Task4

---
+++ Seminar6/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+//Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+Console.Write("1: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("2: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("3: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a < (b + c) && b < (a + c) && c < (b + a))
+{
+    Console.WriteLine("Треугольник");
+}
+
+else Console.WriteLine("Не треугольник");
---
+++ Seminar6/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//45 -> 101101
+//3  -> 11
+//2  -> 10
+
+Console.WriteLine("Введите число ");
+int dec = int.Parse(Console.ReadLine());
+string binary = "";
+
+while(dec != 0)
+{
+    int mod = dec % 2;
+    binary = mod.ToString() + binary;
+    dec = dec / 2;
+}
+
+Console.WriteLine(binary);
