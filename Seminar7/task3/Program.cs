1f2099216253c42a0eab5a51381046a457d9e84b
Pahomob Artem <art.pa99@mail.ru>
2023-07-18 17:11:11 +0300

    task3

---
+++ Seminar7/task3/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+//Выведите полученный массив на экран.
+//m = 3, n = 4.
+//0 1 2 3
+//1 2 3 4
+//2 3 4 5
+
+Console.Write("Введите кол-во строк: " );
+int countRow = int.Parse( Console.ReadLine() );
+
+Console.Write("Введите кол-во столбцов: " );
+int countColumn = int.Parse( Console.ReadLine() );
+
+int[,] array = new int[countRow, countColumn];
+
+// заполнение масива
+for ( int i = 0; i < countRow; i++) // двигается по строками
+{
+    for ( int j = 0; j < countColumn; j++ ) // двигается по столбцам
+    {
+        array[i, j] = i + j;
+    }
+}
+
+// Вывод массива в консоль
+for ( int i = 0; i < countRow; i++)
+{
+    for ( int j = 0; j < countColumn; j++ )
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
