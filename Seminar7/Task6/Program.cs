4aa0e2ece84bbce93fd9ac25500282b092aa9b6d
Pahomob Artem <art.pa99@mail.ru>
2023-07-18 23:27:54 +0300

    task6

---
+++ Seminar7/Task6/Program.cs
@@ -0,0 +1,41 @@
+﻿// Есть двумерный массив заполненный случайными числами, вывести средне арифметичкое сложение каждого столбца.
+
+Console.Write("Введите кол-во строк: " );
+int countRow = int.Parse( Console.ReadLine() );
+
+Console.Write("Введите кол-во столбцов: " );
+int countColumn = int.Parse( Console.ReadLine() );
+
+int[,] array = new int[countRow, countColumn];
+
+
+// Заполнение массива случайными числами (построчно)
+for ( int i = 0; i < countRow; i++) // двигается по строками
+{
+    for ( int j = 0; j < countColumn; j++ ) // двигается по столбцам
+    {
+        array[i, j] = new Random().Next( 1, 11 );
+    }
+}
+// Вывод массива в консоль
+for ( int i = 0; i < countRow; i++)
+{
+    for ( int j = 0; j < countColumn; j++ )
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine(""); // Разделяем
+
+// Вывод средне арифметического
+for ( int i = 0; i < countRow; i++)
+{
+    double sum = 0;
+    for ( int j = 0; j < countColumn; j++ )
+    {
+        sum += array[j, i];     
+    }
+    Console.WriteLine($"Cреднее арифметическое : {sum / countRow}");
+}
