873583e833514baa18842917909a124c820b6bbb
Pahomob Artem <art.pa99@mail.ru>
2023-07-18 18:48:06 +0300

    task4

---
+++ Seminar7/task4/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 49: Задайте двумерный массив. 
+//Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+Console.Write("Введите кол-во строк: " );
+int countRow = int.Parse( Console.ReadLine() );
+
+Console.Write("Введите кол-во столбцов: " );
+int countColumn = int.Parse( Console.ReadLine() );
+
+int[,] array = new int[countRow, countColumn];
+
+// генерируем числа
+for ( int i = 0; i < countRow; i++) // двигается по строками
+{
+    for ( int j = 0; j < countColumn; j++) // двигается по столбцам
+    {
+        array[i, j] = new Random().Next(1, 12);
+    }
+}
+// выводим
+for ( int i = 0; i < countRow; i++)
+{
+    for ( int j = 0; j < countColumn; j++ )
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine(""); // Разделяем
+// выполняем условие
+for ( int i = 0; i < countRow; i++) // двигается по строками
+{
+    for ( int j = 0; j < countColumn; j++) // двигается по столбцам
+    {
+        if (i % 2 == 0)
+        {
+        array[i, j] = array[i, j] * array[i, j]; 
+        }
+        Console.Write(array[i, j] + " ") ;
+        
+    }
+    Console.WriteLine();
+}
