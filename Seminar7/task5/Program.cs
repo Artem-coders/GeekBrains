f8789aaf81dbaa8703bd08cebd7c0f60f0827a74
Pahomob Artem <art.pa99@mail.ru>
2023-07-18 22:30:50 +0300

    task5

---
+++ Seminar7/task5/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Write("Введите кол-во строк: " );
+int countRow = int.Parse( Console.ReadLine() );
+
+Console.Write("Введите кол-во столбцов: " );
+int countColumn = int.Parse( Console.ReadLine() );
+
+int[,] array = new int[countRow, countColumn];
+
+// Заполнение массива случайными числами (построчно)
+for ( int i = 0; i < countRow; i++) // двигается по строками
+{
+    for ( int j = 0; j < countColumn; j++ ) // двигается по столбцам
+    {
+        array[i, j] = new Random().Next( 1, 11 );
+    }
+}
+// Вывод массива в консоль
+for ( int i = 0; i < countRow; i++)
+{
+    for ( int j = 0; j < countColumn; j++ )
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine(""); // Разделяем
+
+// Вывод суммы элементов каждой строки
+for ( int i = 0; i < countRow; i++)
+{
+    int sum = 0;
+    for ( int j = 0; j < countColumn; j++ )
+    {
+        sum = array [0, 0] + array [1,1] + array [2, 2];
+    }
+    Console.WriteLine($"{array [0, 0]} + {array [1,1]} + {array [2, 2]}. Cумма элентов равна : {sum}");
+}
