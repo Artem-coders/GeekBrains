567a12bea205f07cd9a6198262d3735e45caedcb
Pahomob Artem <art.pa99@mail.ru>
2023-07-18 16:29:39 +0300

    task1

---
+++ Seminar7/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте двумерный масиив размером m * n , заполненый случайными целыми числами
+
+Console.Write("Введите кол-во строк: " );
+int countRow = int.Parse( Console.ReadLine() );
+
+Console.Write("Введите кол-во столбцов: " );
+int countColumn = int.Parse( Console.ReadLine() );
+
+int[,] array = new int[countRow, countColumn];
+
+// Заполнение массива случайными числами (построчно)
+for ( int i = 0; i < countRow; i++) // двигается по строками
+{
+    for ( int j = 0; j < countColumn; j++ ) // двигается по столбцам
+    {
+        array[i, j] = new Random().Next( 1, 11 );
+    }
+}
+// Вывод массива в консоль
+for ( int i = 0; i < countRow; i++)
+{
+    for ( int j = 0; j < countColumn; j++ )
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
